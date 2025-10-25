27414403e94132fe71ad74cdbbd08382f878f50b
Elliot Nez Arredondo <a01796107@tec.mx>
2025-10-24 22:07:09 -0600

    Update crud_records.py
    Nuevo archivo, records

--- app/crud_records.py
+++ app/crud_records.py
@@ -1,3 +1,45 @@
-'''
-hello
-''''
+from app.db import get_connection
+
+def get_all_records():
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        cursor.callproc("sp_read_health")
+        result = cursor.fetchall()
+    conn.close()
+    return result
+
+
+def get_record_by_id(record_id: int):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        cursor.callproc("sp_read_health", (health_id,))
+        result = cursor.fetchone()  # uno solo
+    conn.close()
+    return result if result else {"message": "Record not found"}
+
+
+def create_record(babyid, date, vaccine, notes):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        cursor.callproc("sp_create_health", (babyid, date, vaccine, notes))
+        conn.commit()
+    conn.close()
+    return {"message": "Record created successfully"}
+
+
+def update_record(id, date, vaccine, notes):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        cursor.callproc("sp_update_health", (id,date, vaccine, records))
+        conn.commit()
+    conn.close()
+    return {"message": "Record updated successfully"}
+
+
+def delete_record(id):
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        cursor.callproc("sp_delete_health", (id,))
+        conn.commit()
+    conn.close()
+    return {"message": "record deleted successfully"}
